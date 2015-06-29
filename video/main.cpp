12733f06c6bb504830b2470401f1ac58392de902
Lucas Ribeiro de Abreu <Lucas@Lucass-MacBook-Pro.local>
2015-06-28 22:30:34 -0300

    Initial Commit

---
+++ video/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  video
+//
+//  Created by Lucas Ribeiro de Abreu on 28/06/2015.
+//  Copyright (c) 2015 Lucas Ribeiro de Abreu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
