c3679faab22ae81ba11e036f9f61dd0e94830fe0
Lucas Ribeiro de Abreu <Lucas@Lucass-MacBook-Pro.local>
2015-07-29 15:02:39 -0300

    removed imghandler

--- video/main.cpp
+++ video/main.cpp
@@ -13,7 +13,6 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/nonfree/nonfree.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
-#include "imgHandler.cpp"
 #include <string.h>
 
 using namespace cv;
